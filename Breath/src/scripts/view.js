003463b2aaf3c05dae572b0acec201e73c7572ee
ashes4trees <stephsoni18@gmail.com>
2021-12-04 15:21:14 -0500

    Change to pointer lock controls

--- Breath/src/scripts/view.js
+++ Breath/src/scripts/view.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { mouseCoords } from './mouse';
 import { FirstPersonControls } from "three/examples/jsm/controls/FirstPersonControls";
+import { PointerLockControls} from "three/examples/jsm/controls/PointerLockControls";
+// import { DirectionalLight } from 'three';
 
 const ctxWidth = 900;
 const ctxHeight = 506; 
@@ -9,32 +11,38 @@ let clock = new THREE.Clock();
 class View {
     constructor() {
         this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0x6FA8DC);
         this.camera = new THREE.PerspectiveCamera(75, ctxWidth/ctxHeight, 0.1, 1000);
         const canvas = document.querySelector('#image-canvas');
         this.renderer = new THREE.WebGLRenderer({ canvas });
+        this.renderer.physicallyCorrectLights = true;
+        // this.renderer.setClearColor(0x6FA8DC, 0.5);
         
-        this.controls = new FirstPersonControls(this.camera, canvas);
-        this.controls.lookSpeed = 0.1
-        this.controls.movementSpeed = 10
+        this.controls = new PointerLockControls(this.camera, this.renderer.domElement);
+        
+        // this.controls = new FirstPersonControls(this.camera, canvas);
+        // this.controls.lookSpeed = 0.1
+        // this.controls.movementSpeed = 10
         this.camera.position.set(0, 20, 100);
     
-        this.controls.update(clock.getDelta());
+        // this.controls.update(clock.getDelta());
         this.camera.position.z = 10;
 
         this.renderer.setSize(ctxWidth, ctxHeight);
 
-        // const axes = new THREE.AxesHelper(100);
-        // this.scene.add(axes);
+        const axes = new THREE.AxesHelper(100);
+        this.scene.add(axes);
 
         const container = document.querySelector('.canvas-container');
         container.appendChild(this.renderer.domElement);
 
         this.addGround();
+        this.addLight();
     }
 
     addGround() {
         const geometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
-        const mat = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true});
+        const mat = new THREE.MeshStandardMaterial({color: 0x385C28, wireframe: true});
         const ground = new THREE.Mesh(geometry, mat);
         this.scene.add(ground);
         ground.rotateX(- Math.PI / 2);
@@ -43,8 +51,17 @@ class View {
         ground.position.y = -5;
     }
 
+    addLight() {
+        const light = new THREE.AmbientLight(0x111111, 1);
+        const sunLight = new THREE.DirectionalLight(0xfff1e0, 1);
+        this.scene.add(light);
+        this.scene.add(sunLight);
+
+    }
+
     animate() {
-        this.controls.update(clock.getDelta());
+        // this.controls.update(clock.getDelta());
+        this.controls.lock();
         requestAnimationFrame(this.animate.bind(this)); 
         this.renderer.render(this.scene, this.camera);
         this.getPos();
