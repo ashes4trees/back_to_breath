96dfecaab093b8f04db5fb46a56170bae1507e94
ashes4trees <89805061+ashes4trees@users.noreply.github.com>
2022-01-26 22:48:03 -0500

    fix import path for textures

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -89,7 +89,7 @@ class View {
         const z = (Math.random() * 200) - 100;
 
         const shape = new THREE.CircleBufferGeometry(5, 2, 0, 360);
-        const tex = new THREE.TextureLoader().load('./src/assets/leaf.jpg');
+        const tex = new THREE.TextureLoader().load('../src/assets/leaf.jpg');
         const mats = new THREE.MeshStandardMaterial({ map: tex });
         this.leaf = new THREE.Mesh(shape, mats);
         this.leaf.name = 'leaf';
