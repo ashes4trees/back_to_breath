33c6492498973e93ca452698bd56155ef308ed14
ashes4trees <89805061+ashes4trees@users.noreply.github.com>
2022-01-26 22:40:26 -0500

    debug ground texture

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -40,7 +40,7 @@ class View {
        
         const geometry = new THREE.PlaneBufferGeometry(5000, 5000, 100, 100);
        
-        const texture = new THREE.TextureLoader().load('./src/assets/grass_path.jpg');
+        const texture = new THREE.TextureLoader().load('../src/assets/grass_path.jpg');
         texture.wrapS = THREE.RepeatWrapping;
         texture.wrapT = THREE.RepeatWrapping;
         texture.repeat.set(32, 32);
